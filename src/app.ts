d6ddadf1f4702a804dc7b2672d6681559fcac111
pelumini <pelumini@gmail.com>
2022-07-15 06:55:31 +0100

    cors settings changed

--- src/app.ts
+++ src/app.ts
@@ -12,12 +12,14 @@ import deserializeUser from './middleware/deserializeUser';
 const port = process.env.PORT || config.get<number>('port');
 const app = express();
 
-app.use(
-  cors({
-    origin: config.get('origin'),
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: config.get('origin'),
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
